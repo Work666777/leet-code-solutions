35d28ed6fb45f6e6006367a0df1048e59c82356b
Work666777 <vaka08258@gmail.com>
2026-09-09 17:47:40 +0300

    fix

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -16,7 +16,7 @@ class Solution:
 
         for n in nums:
             count[n] = 1+ count.get(n, 0)
-            for n, c in count.items():
+        for n, c in count.items():
                 fre[c].append[n]
                 
 
