543daf0eebb6bfd38239c6247e1aefe26b4bf1ca
Work666777 <vaka08258@gmail.com>
2026-09-09 17:30:45 +0300

    Add Top K Frequent Elements solution

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -5,4 +5,11 @@ class Solution:
             res[l] = 1 + res.get(l, 0)
         sorted_res = sorted(res.keys(), key=lambda x: res[x], reverse=True)
         return sorted_res[:k]
-        
+
+
+        # or more efficient solution 
+
+    class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        fre = [[] for i in rane(len(nums)), + 1]
