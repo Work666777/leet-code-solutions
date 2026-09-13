f6bb6f3ba1032f6c347a321341399cc496bec1c0
Work666777 <vaka08258@gmail.com>
2026-09-13 18:11:22 +0300

    unsuccessful try

---
+++ fsdiufhusif.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+       # while ii < len(nums):
+        # ii = 0  
+        res = {}
+        ii = 0
+        nums = sorted(nums)
+        for i, n in enumerate(nums):
+            #if int(i) < len(nums):
+            if i + 1 < len(nums):
+                if (nums[i] == nums[i+1]) or (i > 0 and nums[i] == nums[i-1]):
+                    continue
+                
+                #if nums[i] == nums[i+1] or nums[i-1]:
+                    continue
+                elif  nums[i] + 1 == nums[i+1]:
+                    res[ii] += nums[i]
+                elif nums[i] + 1 != nums[i+1]:
+                    ii += 1
+        return res
+
+sol = Solution()
+print(sol.longestConsecutive([100, 4, 200, 1, 3, 2]))
